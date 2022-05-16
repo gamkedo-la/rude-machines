dabd889d8386c3ca3243ee4e51fa3496522e9396
liyiz <5160472+liyiz@users.noreply.github.com>
2022-05-16 20:04:01 +0100

    add TestEnemy prefab, add SpawnDefaultEnemy functionality to "/" key input
    You can now spawn in a test enemy prefab with the "/" key. It spawns in at a hardcoded vector that is relative to player position, but not relative to direction player is facing. I'll try to figure out how to do that.

--- Assets/Scripts/Scr_DebugSpawner.cs
+++ Assets/Scripts/Scr_DebugSpawner.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Scr_DebugSpawner : MonoBehaviour
 {
+
+    public GameObject defaultEnemy;
+    private Scr_PlayerController playerController;
+
     // Start is called before the first frame update
     void Start()
     {
-
+      playerController = GetComponent<Scr_PlayerController>();
     }
 
     // Update is called once per frame
@@ -16,8 +21,16 @@ public class Scr_DebugSpawner : MonoBehaviour
 
     }
 
-    public void SpawnDefaultEnemy()
+    public void SpawnDefaultEnemy(InputAction.CallbackContext context)
     {
-      Debug.Log("BOOP");
+      if (context.started)
+      {
+        // GameObject newDefaultEnemy = Instantiate(...);
+        float rot_y = playerController.transform.rotation.y;
+        Vector3 pos = playerController.transform.position + new Vector3(0,4,8);
+        Quaternion rot = Quaternion.Euler(0, rot_y, 0);
+        // Debug.Log("BOOP " + rot);
+        Instantiate(defaultEnemy, pos, rot);
+      }
     }
 }
