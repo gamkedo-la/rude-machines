5c05873e58821422685d90523067fc5983af4883
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2022-06-09 21:02:43 +0500

    Enemy Manager (WIP)

--- Assets/Scripts/Managers/Scr_EnemyManager.cs
+++ Assets/Scripts/Managers/Scr_EnemyManager.cs
@@ -10,7 +10,8 @@ public class Scr_EnemyManager : MonoBehaviour
         public string name;
         public GameObject prefab;
         public Vector3 offset;
-        public List<GameObject> enemies = new List<GameObject>();
+        
+        [HideInInspector] public List<GameObject> enemies = new List<GameObject>();
     }
 
     [System.Serializable]
@@ -26,20 +27,57 @@ public class Scr_EnemyManager : MonoBehaviour
         public float checkAfterTime;
         public int checkForAmount; //-1 means infinite
         [Space]
-        public string[] conditionType;
-        public int[] conditionValue;
-        [Space]
-        public string[] key;
+        //public string[] conditionTypes;
+        //public int[] conditionValues;
+        //[Space]
+        public string[] keysWithTransformIndex;
 
         [HideInInspector] public float timer = 0.0f;
     }
 
+    [SerializeField] private List<EnemyKey> keys = new List<EnemyKey>();
+    [Space]
     public int currentWave = 0;
     [SerializeField] private List<Wave> waves = new List<Wave>();
+
+    private float enemyKeyListTimer = 0.0f;
 
     void Start()
     {
         
+    }
+
+    EnemyKey GetKey(string name)
+    {
+        foreach(var key in keys)
+        {
+            if(key.name.Contains(name))
+            {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    void RemoveDestroyedFromAllKeys()
+    {
+        if(enemyKeyListTimer <= 0.0f)
+        {
+            List<GameObject> destroyedEnemies = new List<GameObject>();
+            foreach(var key in keys)
+            {
+                foreach(var enemy in key.enemies)
+                {
+                    destroyedEnemies.Add(enemy);
+                }
+                foreach(var destroyedEnemy in destroyedEnemies)
+                {
+                    key.enemies.Remove(destroyedEnemy);
+                }
+                destroyedEnemies.Clear();
+            }
+            enemyKeyListTimer = 1.0f;
+        }
     }
 
     void Update()
@@ -49,9 +87,17 @@ public class Scr_EnemyManager : MonoBehaviour
         {
             if(command.timer <= 0.0f)
             {
-                if(command.checkForAmount == 0) commandsToRemove.Add(command);
-                else command.checkForAmount--;
+                foreach(var keyTransform in command.keysWithTransformIndex)
+                {
+                    EnemyKey eKey = GetKey(keyTransform);
+                    if(eKey != null)
+                    {
+                        Transform t = transform.GetChild(System.Int32.Parse(keyTransform.Replace(eKey.name, "")));
+                        eKey.enemies.Add(Instantiate(eKey.prefab, t.position + eKey.offset, t.rotation));
+                    }
+                }
 
+                if(--command.checkForAmount == 0) commandsToRemove.Add(command);
                 command.timer = command.checkAfterTime;
             }
             else
