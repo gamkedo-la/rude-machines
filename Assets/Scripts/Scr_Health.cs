c98aa93a6eb46fb6ca8f3a7a7c78937a6af86c0d
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2022-06-12 19:47:09 +0500

    Integrated Damage and Destroy SFXs.

--- Assets/Scripts/Scr_Health.cs
+++ Assets/Scripts/Scr_Health.cs
@@ -5,11 +5,21 @@ public class Scr_Health : Scr_BarProperty, Scr_IDamageable
     [Space]
     public float damageFactor = 1.0f;
     public GameObject damageEffect = null;
+    public AudioClip[] damageSFX;
+    [Space]
     public GameObject destroyEffect = null;
     public Vector3 destroyEffectScale = Vector3.one;
+    public AudioClip destroySFX;
     [Space]
     public Scr_Detect detector = null;
     public float pauseDetectorForTime = 0.0f;
+
+    private AudioSource audSrc;
+
+    void Start()
+    {
+        audSrc = GetComponent<AudioSource>();
+    }
 
     virtual public void Damage(float value, GameObject instigator)
     {
@@ -26,18 +36,20 @@ public class Scr_Health : Scr_BarProperty, Scr_IDamageable
         if(damageEffect != null)
             damageEffect.SetActive(true);
 
-        if(Value <= 0)
+        if(Value <= 0.0f)
         {
-            if(isPlayer)
-            {
-                Scr_GameManager.instance.SetStuckTime(0.08f, 0.04f);
-            }
-
+            if(isPlayer) Scr_GameManager.instance.SetStuckTime(0.08f, 0.04f);
             if(destroyEffect != null)
             {
-                Instantiate(destroyEffect, transform.position, Quaternion.identity).transform.localScale = destroyEffectScale;
+                GameObject newDestroyEffect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
+                newDestroyEffect.transform.localScale = destroyEffectScale;
+                newDestroyEffect.GetComponent<AudioSource>().PlayOneShot(destroySFX);
             }
             Destroy(gameObject);
+        }
+        else
+        {
+            audSrc.PlayOneShot(damageSFX[Random.Range(0, damageSFX.Length)]);
         }
     }
 }
